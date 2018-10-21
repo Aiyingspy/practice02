92ba23dbce738affd9d9d7d56a645fe9fc133f4e
Aiyingspy <707135375@qq.com>
2018-10-21 14:02:12 +0800

    forth modified

---
+++ app/src/main/java/com/example/weatherapp/com/example/weatherapp/bean/City.java
@@ -0,0 +1,43 @@
+package com.example.weatherapp.com.example.weatherapp.bean;
+
+public class City {
+    private String province;
+    private String city;
+    private String number;
+    private String firstPY;
+    private String allPY;
+    private String allFristPY;
+    public City(String province, String city, String number, String
+            firstPY, String allPY, String allFristPY) {
+        this.province = province;
+        this.city = city;
+        this.number = number;
+        this.firstPY = firstPY;
+        this.allPY = allPY;
+        this.allFristPY = allFristPY;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getFirstPY() {
+        return firstPY;
+    }
+
+    public String getAllPY() {
+        return allPY;
+    }
+
+    public String getAllFristPY() {
+        return allFristPY;
+    }
+}
