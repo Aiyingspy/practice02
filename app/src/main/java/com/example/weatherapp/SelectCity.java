39c78ccbf8b5b0258dbac38dc9edc7f40637efca
Aiyingspy <707135375@qq.com>
2018-11-12 23:06:18 +0800

    City Search Function

--- app/src/main/java/com/example/weatherapp/SelectCity.java
+++ app/src/main/java/com/example/weatherapp/SelectCity.java
@@ -3,9 +3,14 @@ package com.example.weatherapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -14,6 +19,7 @@ import android.widget.Toast;
 import com.example.weatherapp.app.MyApplication;
 import com.example.weatherapp.com.example.weatherapp.bean.City;
 
+import android.widget.ListView;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,22 +28,68 @@ public class SelectCity extends Activity implements View.OnClickListener {
     private ListView mlistView;
     private TextView title_name;
     private List<City> mCityList;
+    private EditText mEditText;
+    private Button searchBtn;
+
+
+
     private List<City> mCityList_on;
-   // private Myadapter myadapter;
+    // private Myadapter myadapter;
     private int xiabiao;
     List<String> listOfStringName = new ArrayList<String>();
     List<String> listOfStringCode = new ArrayList<String>();
-    protected void onCreate(Bundle saveInstanceState){
+    List<String> listOfStringRefrsh = new ArrayList<String>();
+
+    protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.select_city);
+        mEditText = (EditText) findViewById(R.id.search_edit);
+        mBackBtn = (ImageView) findViewById(R.id.title_back);
+        mBackBtn.setOnClickListener(this);
+        title_name = (TextView) findViewById(R.id.title_name);
+        mlistView = (ListView) findViewById(R.id.list_view);
+        MyApplication myApplication = (MyApplication) getApplication();
+        mCityList = myApplication.getCityList();
 
-        initView();
+        searchBtn = (Button) findViewById(R.id.button);
+        searchBtn.setOnClickListener(this);
+
+        for (City city : mCityList) {
+            listOfStringName.add(city.getCity());
+            listOfStringCode.add(city.getNumber());
+
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                SelectCity.this, android.R.layout.simple_list_item_1, listOfStringName);
+        adapter.notifyDataSetChanged();
+        mlistView.setAdapter(adapter);
+
+//        mListViewAdapter=new ListViewAdapter(this, listOfStringName);
+//        //过滤数据
+//        //过滤出姓名里面包含"张"的数据
+//       mListViewAdapter.getFilter().filter(inputText);
+//        mlistView.setAdapter(mListViewAdapter);
+
+
+        mlistView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                xiabiao = i;
+                // title_name
+                title_name.setText("当前城市：" + listOfStringName.get(xiabiao));
+                Toast.makeText(SelectCity.this,
+                        "你单击了:" + i,
+
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
- //   private String[]  data={"第一组","第二组","第三组","第四组"};
+
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.title_back:
                 Intent i = new Intent();
 
@@ -45,41 +97,34 @@ public class SelectCity extends Activity implements View.OnClickListener {
                 setResult(RESULT_OK, i);
                 finish();
                 break;
+            case R.id.button:
+                String inputText = mEditText.getText().toString();
+                listOfStringName.clear();
+                listOfStringCode.clear();
+                for (int j = 0; j < mCityList.size(); j++) {
+
+                    String number = mCityList.get(j).getNumber();
+
+                    String cityName = mCityList.get(j).getCity();
+                    String Pin = mCityList.get(j).getAllFristPY();
+
+                    if (number.equals(inputText) || cityName.equals(inputText) || Pin.equals(inputText)) {
+
+                        listOfStringName.add(cityName);
+                        listOfStringCode.add(number);
+                    }
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                            SelectCity.this, android.R.layout.simple_list_item_1, listOfStringName);
+                    adapter.notifyDataSetChanged();
+                    mlistView.setAdapter(adapter);
+                }
+
             default:
                 break;
         }
 
     }
-    private void initView(){
-        mBackBtn=(ImageView)findViewById(R.id.title_back);
-        mBackBtn.setOnClickListener(this);
-        title_name= (TextView) findViewById(R.id.title_name);
-        mlistView = (ListView)findViewById(R.id.list_view);
-        MyApplication myApplication=(MyApplication) getApplication();
-        mCityList= myApplication.getCityList();
-        for(City city:mCityList)
-        {
-          //  mCityList_on.add(city);
-            listOfStringName.add(city.getCity());
-            listOfStringCode.add(city.getNumber());
-
-        }
-        //myadapter = new Myadapter(SelectCity.this,mCityList);
-        ArrayAdapter<String> adapter=new ArrayAdapter<String>(
-                SelectCity.this,android.R.layout.simple_list_item_1,listOfStringName);
-        mlistView.setAdapter(adapter);
-        mlistView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                xiabiao=i;
-               // title_name
-                title_name.setText("当前城市："+listOfStringName.get(xiabiao));
-                Toast.makeText(SelectCity.this,
-                        "你单击了:"+i,
-
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
 }
+
+
+
